7bb30aa68d22a91b71652718ca0cb29213f91d9c
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 21:06:59 +0100

    feat: added route for user to view profile detail

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,12 @@ const express = require('express')
 const userRouter = express.Router();
 const User = require('../models/User');
 const { registerValidation, loginValidation } = require('../validations/UserValidation');
-const { register, login } = require('../controllers/AuthController');
+const { register, login, get } = require('../controllers/AuthController');
+const verifyToken = require('../middlewares/verifyToken');
 
 
 userRouter.post('/register', registerValidation, register).post('/login', loginValidation, login);
 
+userRouter.get('/', verifyToken, get);
 
 module.exports = userRouter
