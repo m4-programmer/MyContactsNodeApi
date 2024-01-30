ba4f31a873d61144d19351a8fc3a665f3edaa444
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 23:14:24 +0100

    feat: implemented controller to fetch all contacts

---
+++ controllers/ContactController.js
@@ -0,0 +1,17 @@
+const Contact = require('../models/Contact');
+const { success, failure } = require('../utils/Helper');
+
+// get all contacts
+const get = async (req, res) => {
+    try {
+        const contacts = await Contact.find();
+        return res.status(200).json(success('Contacts fetched successfully', contacts, 200));
+    } catch (error) {
+        return res.status(500).json(failure('Something went wrong', error, 500));
+    }
+};
+
+
+module.exports = {
+    get
+}
