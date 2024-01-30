ac94a61be835be887ee7ba234c1599c7366d1591
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 23:27:57 +0100

    feat: implemented logic to create contact

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const Contact = require('../models/Contact');
 const { success, failure } = require('../utils/Helper');
 
@@ -11,7 +12,42 @@ const get = async (req, res) => {
     }
 };
 
+//create contact
+const store = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json(failure('Validation failed', errors.array(), 422));
+    }
+    const { name, email, phone } = req.body;
+    
+    try {
+        const contact = await Contact.create({ name, email, phone, user_id: req.user._id });
+        return res.status(201).json(success('Contact created successfully', contact, 201));
+    } catch (error) {
+        return res.status(500).json(failure('Something went wrong', error, 500));
+    }
+}
+
+//update contact details
+const update = async (req, res) => {
+    
+}
+
+//delete contact
+const destroy = async (req, res) => {
+    
+}
+
+//get single contact that belongs to a user
+const show = async (req, res) => {
+    
+}
+
 
 module.exports = {
-    get
+    get,
+    store,
+    update,
+    destroy,
+    show
 }
