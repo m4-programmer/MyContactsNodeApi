3122a865e0b926bea433daf3c038f7bac7c24087
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 20:16:46 +0100

    feat: implemented login controller logic

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -27,7 +27,7 @@ const register = async (req,res) => {
      return res.status(201).json(success('User created Successfully', { token }, 201));
 
    } catch (err) {
-        console.log(err);
+        
         return res.status(500).json(failure('Something went wrong', err, 500));
    }
 }
@@ -35,6 +35,32 @@ const register = async (req,res) => {
 
 
 //login user
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json(failure('Validation failed', errors.array(), 422));
+    }
+    try {
+        //hash password
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+
+        const user = await User.getByEmail(email, true);
+        
+        //check password
+        if (!user || !bcrypt.compareSync(password, user.password)) {
+            return res.status(404).json(failure('Incorrect Email or Password', [], 404));
+        }
+        
+        //generate token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '2m' });
+        //return response
+        return res.status(200).json(success('User logged in successfully', { token }, 200));
+    } catch (error) {
+        return res.status(500).json(failure('Something went wrong', error, 500));
+    }
+}
 
 
 //get user details/profile
@@ -46,4 +72,4 @@ const register = async (req,res) => {
 
 //logout user
 
-module.exports = {register}
+module.exports = {register, login}
