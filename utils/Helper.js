0459186c8602ce917f5cbd200e0777c9b58daf13
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 15:23:10 +0100

    fix: exported constants

--- utils/Helper.js
+++ utils/Helper.js
@@ -1,2 +1,4 @@
 const success  = (message, data = [] ,code = 200) => ({ success: true,status: code, message, data });
 const failure  = ( message = "", error = [], code = 500 ) => ({ success: false, status: code, message, error });
+
+module.exports = {success, failure};
