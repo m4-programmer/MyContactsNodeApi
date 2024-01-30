bb342fa4d2974561c9651716c8afc2ac56726bed
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 23:34:51 +0100

    feat: created route to update contact details

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,12 +3,12 @@ const express = require('express')
 const contactRouter = express.Router();
 const Contact = require('../models/Contact');
 const { success, failure } = require('../utils/Helper');
-const { get, store } = require('../controllers/ContactController');
+const { get, store, update } = require('../controllers/ContactController');
 const verifyToken = require('../middlewares/verifyToken');
 const { ContactValidation } = require('../validations/ContactValidation');
 
 
 contactRouter.get('/', verifyToken, get);
-contactRouter.post('/', verifyToken, ContactValidation, store);
+contactRouter.post('/', verifyToken, ContactValidation, store).put('/:id', verifyToken, ContactValidation, update);
 
 module.exports = contactRouter
