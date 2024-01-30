776a26878796ca5b1c4a20fa33ca58ec8aecd9c1
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 21:36:36 +0100

    feat: register route to update profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,12 @@ const express = require('express')
 const userRouter = express.Router();
 const User = require('../models/User');
 const { registerValidation, loginValidation } = require('../validations/UserValidation');
-const { register, login, get } = require('../controllers/AuthController');
+const { register, login, get, update } = require('../controllers/AuthController');
 const verifyToken = require('../middlewares/verifyToken');
 
 
 userRouter.post('/register', registerValidation, register).post('/login', loginValidation, login);
 
-userRouter.get('/', verifyToken, get);
+userRouter.get('/', verifyToken, get).put('/', verifyToken, update);
 
 module.exports = userRouter
