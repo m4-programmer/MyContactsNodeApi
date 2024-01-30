6fb60b55c09d36a18e60907f95d67748b3f03789
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 23:15:51 +0100

    fix: added logic to fetch contacts that belongs to authenticated user

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -4,7 +4,7 @@ const { success, failure } = require('../utils/Helper');
 // get all contacts
 const get = async (req, res) => {
     try {
-        const contacts = await Contact.find();
+        const contacts = await Contact.find({ user_id: req.user._id });
         return res.status(200).json(success('Contacts fetched successfully', contacts, 200));
     } catch (error) {
         return res.status(500).json(failure('Something went wrong', error, 500));
