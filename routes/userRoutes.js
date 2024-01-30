9115c4c45a6a93cdad613f386e5bfe677a380b0e
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 20:16:30 +0100

    feat: created login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,11 @@ const express = require('express')
 
 const userRouter = express.Router();
 const User = require('../models/User');
-const { registerValidation } = require('../validations/UserValidation');
-const { register } = require('../controllers/AuthController');
+const { registerValidation, loginValidation } = require('../validations/UserValidation');
+const { register, login } = require('../controllers/AuthController');
 
 
-userRouter.post('/register', registerValidation, register);
+userRouter.post('/register', registerValidation, register).post('/login', loginValidation, login);
 
 
 module.exports = userRouter
