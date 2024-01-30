02db3b84616af63a415eb9816bf481db0defa9e1
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 14:03:27 +0100

    fix: added more constants

--- constants.js
+++ constants.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+const ENV = process.env.ENV || 'local';
+const APP_URL = process.env.APP_URL || 'http://localhost:3000';
 
-
-module.exports = { PORT, MONGO_URI }
+module.exports = { PORT, MONGO_URI, ENV, APP_URL }
