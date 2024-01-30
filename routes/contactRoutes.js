8b9c296aa06607430d0187342c88cae7c54df82e
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 23:23:12 +0100

    feat: created route to create a contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,10 +3,12 @@ const express = require('express')
 const contactRouter = express.Router();
 const Contact = require('../models/Contact');
 const { success, failure } = require('../utils/Helper');
-const { get } = require('../controllers/ContactController');
+const { get, store } = require('../controllers/ContactController');
 const verifyToken = require('../middlewares/verifyToken');
+const { ContactValidation } = require('../validations/ContactValidation');
 
 
 contactRouter.get('/', verifyToken, get);
+contactRouter.post('/', verifyToken, ContactValidation, store);
 
 module.exports = contactRouter
