1305dfa14d8d2ba80ba9961b467f8e272587a2d7
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 22:09:39 +0100

    feat: created route for changing user password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,13 +2,14 @@ const express = require('express')
 
 const userRouter = express.Router();
 const User = require('../models/User');
-const { registerValidation, loginValidation } = require('../validations/UserValidation');
-const { register, login, get, update } = require('../controllers/AuthController');
+const app = express()
+const { registerValidation, loginValidation, changePasswordValidation } = require('../validations/UserValidation');
+const { register, login, get, update, changePassword } = require('../controllers/AuthController');
 const verifyToken = require('../middlewares/verifyToken');
 
 
 userRouter.post('/register', registerValidation, register).post('/login', loginValidation, login);
 
 userRouter.get('/', verifyToken, get).put('/', verifyToken, update);
-
+userRouter.put('/password', verifyToken, changePasswordValidation, changePassword);
 module.exports = userRouter
