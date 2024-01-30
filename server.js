bf1146110064f3368d4755b5661e3c9b7d814bff
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 18:52:44 +0100

    feat: implemented router:

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const route = require('./routes');
 const Db = require('./utils/Db');
 const { PORT } = require('./constants');
 
@@ -8,4 +8,7 @@ Db.connect();
 const app = express();
 
 app.use(express.json());
+
+route.use('api/v1', route);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
