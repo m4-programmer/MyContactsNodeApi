326a81901cbdc229f52040c0995857933f53f990
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 14:34:02 +0100

    feat: implemented api helper

---
+++ utils/Helper.js
@@ -0,0 +1,2 @@
+const success  = (message, data = [] ,code = 200) => ({ success: true,status: code, message, data });
+const failure  = ( message = "", error = [], code = 500 ) => ({ success: false, status: code, message, error });
