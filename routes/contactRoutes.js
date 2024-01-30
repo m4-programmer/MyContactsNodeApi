9d1cb96246f091a44f4f9342566ea693df855989
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 23:45:19 +0100

    feat: implemented the route for viewing a single contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,14 +3,15 @@ const express = require('express')
 const contactRouter = express.Router();
 const Contact = require('../models/Contact');
 const { success, failure } = require('../utils/Helper');
-const { get, store, update, destroy } = require('../controllers/ContactController');
+const { get, store, update, destroy, show } = require('../controllers/ContactController');
 const verifyToken = require('../middlewares/verifyToken');
 const { ContactValidation } = require('../validations/ContactValidation');
 
 
 contactRouter.get('/', verifyToken, get);
 contactRouter.
-     post('/', verifyToken, ContactValidation, store)
+    get('/:id', verifyToken, show)    
+    .post('/', verifyToken, ContactValidation, store)
     .put('/:id', verifyToken, ContactValidation, update)
     .delete('/:id', verifyToken, destroy);
 
