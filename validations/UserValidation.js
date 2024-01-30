0dc98246c8fe294185177bfa32123c84b9d7f338
Adolphus Miracle <miraboy13@gmail.com>
2024-01-30 19:10:10 +0100

    feat: implemented validation rule for user registration

---
+++ validations/UserValidation.js
@@ -0,0 +1,18 @@
+const {body} = require('express-validator')
+const User = require('../models/User')
+
+const isEmailUnique = async (email) => {
+    console.log(email);
+    const user = await User.getByEmail(email);
+    if (!user) {
+        throw new Error('Email already exists');
+    }
+    return true;
+}
+const registerValidation = [
+    body('name').notEmpty().withMessage("Name is required"),
+    body('email').notEmpty().withMessage("Email is required").custom(isEmailUnique),
+    body('password').isLength({min: 5}).withMessage("Password must be at least 5 characters long").isString(),
+]
+
+module.exports = {registerValidation}
